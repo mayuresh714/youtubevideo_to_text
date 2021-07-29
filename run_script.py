b2cc79941d5d28a81ccacbb40307b6930bdc64e5
Mayuresh123 <56029669+mayuresh714@users.noreply.github.com>
2021-07-29 20:02:24 +0530

    Update run_script.py

--- run_script.py
+++ run_script.py
@@ -5,5 +5,5 @@ TOKEN              = '1403865219:AAHju1lmn-faKj4u3c8tN3GsYpPY33unuHI'
 heroku_app_name    = "botnew123"
 start_msg          = "hi welcome you can start giving the input..."
 
-obj = hd.telegram(TOKEN,heroku_app_name)
+obj = hd.telegram(TOKEN,heroku_app_name,start_msg)
 obj.main()
