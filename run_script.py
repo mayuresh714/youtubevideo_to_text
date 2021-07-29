6cb692ab90ed79a4b27001ca01babaada5a3f4d0
Mayuresh123 <mayureshkhanaj@gmail.com>
2021-07-29 19:51:48 +0530

    changed name to botnew123 for heroku app

--- run_script.py
+++ run_script.py
@@ -1,8 +1,9 @@
 import telg as te
+import heroku_deploy as hd
 
 TOKEN              = '1403865219:AAHju1lmn-faKj4u3c8tN3GsYpPY33unuHI'
-heroku_app_name    = "newtelebot"
+heroku_app_name    = "botnew123"
 start_msg          = "hi welcome you can start giving the input..."
 
-obj = te.telegram(TOKEN,heroku_app_name)
+obj = hd.telegram(TOKEN,heroku_app_name)
 obj.main()
